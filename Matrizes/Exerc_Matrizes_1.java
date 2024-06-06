b83cda8d6a80ed13240a8570d8d3d1fad15d02bf
Eduardo <eduardoodferreiradev@gmail.com>
2024-06-06 13:12:26 -0300

    b

---
+++ Matrizes/Exerc_Matrizes_1.java
@@ -0,0 +1,12 @@
+package Facul.Matrizes;
+import java.util.Scanner;
+public class Exerc_Matrizes_1 {
+    Scanner scanner = new Scanner(System.in);
+
+}
+
+/*
+Exercício 1. Escreva um programa Java que declare uma matriz 5 x 5, preencha
+com 1 a diagonal principal e com 0 os demais elementos e, ao final, mostre a matriz
+obtida.
+ */
