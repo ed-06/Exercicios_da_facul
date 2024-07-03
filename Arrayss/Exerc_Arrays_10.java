4b9275f0d0648c2523559e41ef67bbcab4489391
Eduardo <eduardoodferreiradev@gmail.com>
2024-07-03 19:02:42 -0300

    Arrays 9 - 10

--- Arrayss/Exerc_Arrays_10.java
+++ Arrayss/Exerc_Arrays_10.java
@@ -1,7 +1,33 @@
 package Exercicios_facul.Arrayss;
-
+import java.util.Scanner;
 public class Exerc_Arrays_10 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double vetor [] = new double[15];
+        double melhorTempo = 99999999;
+        double mediaTempo = 0; 
+        double piorTempo = 0;
+        for (int i = 0; i < 15; i++){
+            System.out.print("Tempo da volta: ");
+            vetor[i] = scanner.nextDouble();
+            mediaTempo += vetor[i];
+        }
+        for (int i = 0; i < 15; i++){
+            if (vetor[i] <= melhorTempo ) {
+                melhorTempo = vetor[i];
+            }
+        }
+        for (int i = 0; i < 15; i++){
+            if (vetor[i] >= piorTempo) {
+                piorTempo = vetor[i];
+            }
+        }
+        mediaTempo = mediaTempo / 15;
+        System.out.print("O melhor tempo de voltas é: " + melhorTempo);
+        System.out.print("O pior tempo de voltas é: " + piorTempo);
+        System.out.print("O tempo médio de voltas é: " + mediaTempo);
+        scanner.close();
+    }
 }
 /*
  * Exercício 10. Durante uma corrida de automóveis com N voltas de duração foram
