3138e89df25ba841cdffa5c7481f6e4bc6ae62a0
Eduardo <eduardoodferreiradev@gmail.com>
2024-07-04 14:08:06 -0300

    subrotinas completo

--- Sub_Rotinas/Exerc_Sub_Rotinas_8.java
+++ Sub_Rotinas/Exerc_Sub_Rotinas_8.java
@@ -1,8 +1,30 @@
 package Exercicios_facul.Sub_Rotinas;
-
+import java.util.Scanner;
 public class Exerc_Sub_Rotinas_8 {
-    
-}
+    public static void main (String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Escreva o primeiro número inteiro: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Escreva o primeiro número inteiro: ");
+        int num2 = scanner.nextInt();
+        scanner.close();
+        int soma = calcularSomatorio(num1, num2);
+        System.out.println("Entre os números" + num1 + " e " + num2);
+        System.out.println("A soma é: " + soma);
+    }
+    public static int calcularSomatorio (int num1, int num2){
+        int soma = 0;
+        if (num1 > num2){
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        for (int i = num1; i <= num2; i++){
+            soma += i;
+        }
+        return soma; 
+        }
+    }
 /*
  * 8. Faça um programa em Java que receba do usuário dois números inteiros e
 invoque uma sub-rotina chamada “calcularSomatorio”. Em seguida, crie a sub-rotina
