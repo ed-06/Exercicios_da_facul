b8ae954c9df8c0c89e952bac232a5e47cbf2de95
Eduardo <eduardoodferreiradev@gmail.com>
2024-07-04 00:14:18 -0300

    Classes e objetos 1 - 2 e matrizes pré prova 1 - 2

--- Classes_e_Objetos/Exerc_Classes_e_Objetos_2.java
+++ Classes_e_Objetos/Exerc_Classes_e_Objetos_2.java
@@ -1,14 +1,32 @@
 package Exercicios_facul.Classes_e_Objetos;
-
 public class Exerc_Classes_e_Objetos_2 {
-    
+    public static void main(String[] args) {
+        class Animal {
+            private String especie;
+            private String nomeCientifico;
+            private double pesoMedio;
+
+            public Animal(String especie, String nomeCientifico, double pesoMedio) {
+                this.especie = especie;
+                this.nomeCientifico = nomeCientifico;
+                this.pesoMedio = pesoMedio;
+            }
+
+            public String toString() {
+                return "Espécie: " + especie + ", Nome Científico: " + nomeCientifico + ", Peso Médio: " + pesoMedio + " kg";
+            }
+        }
+
+        // Criando objetos da classe interna Animal
+        Animal animal1 = new Animal("peixe-bolha", "psychrolutes marcidus", 0.6);
+        Animal animal2 = new Animal("ornitorrinco", "ornithorhynchus anatinus", 1.25);
+        Animal animal3 = new Animal("toupeira-nariz-de-estrela", "condylura cristata", 0.3);
+        Animal animal4 = new Animal("uacari-branco", "cacajao calvus", 3.1);
+
+        // Exibindo as informações dos objetos criados
+        System.out.println(animal1);
+        System.out.println(animal2);
+        System.out.println(animal3);
+        System.out.println(animal4);
+    }
 }
-/*
- * 2. Faça um algoritmo que crie uma classe interna referente a um animal contendo
-os atributos espécie, nome científico e peso médio. Em seguida, crie quatro objetos
-dessa classe com os seguintes atributos:
-- objeto 1: peixe-bolha, psychrolutes marcidus, 0.6 kg
-- objeto 2: ornitorrinco, ornithorhynchus anatinus, 1.25 kg
-- objeto 3: toupeira-nariz-de-estrela, condylura cristata, 0.3 kg
-- objeto 4: uacari-branco, cacajao calvus, 3.1 kg
- */
--- Matrizes/Pré_prova/Exerc_Pré_prova_2.java
+++ Matrizes/Pré_prova/Exerc_Pré_prova_2.java
@@ -1,7 +1,20 @@
 package Exercicios_facul.Matrizes.Pré_prova;
-
+import java.util.Scanner;
+import java.util.Random;
 public class Exerc_Pré_prova_2 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numeroAleatorio = random.nextInt(10);
+        String vetor[] = new String[10];
+        for (int i = 0; i < 10; i++){
+            System.out.print("Escreva o nome: ");
+            String nome = scanner.nextLine();
+            vetor[i] = nome;
+        }
+        System.out.print(vetor[numeroAleatorio]);
+        scanner.close();
+    }
 }
 /*
  * Escreva um programa que pergunte os nomes de 10 alunos de uma sala de aula. Armazene
