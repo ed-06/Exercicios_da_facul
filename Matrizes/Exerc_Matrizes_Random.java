e93c5949a5c107fb75c401436a34c7dd15bd8a67
Eduardo <eduardoodferreiradev@gmail.com>
2024-07-19 11:19:56 -0300

    Feito

--- Matrizes/Exerc_Matrizes_Random.java
+++ Matrizes/Exerc_Matrizes_Random.java
@@ -1,6 +1,18 @@
 package Exercicios_facul.Matrizes;
 import java.util.Random;
 public class Exerc_Matrizes_Random {
+    public static boolean verificarRep(int[][] cartela, int num, int linha, int coluna) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i < linha || (i == linha && j < coluna)) {
+                    if (cartela[i][j] == num) {
+                        return true; //achou algum repetido
+                    }
+                }
+            }
+        }
+        return false; // o numero não repitiu então bye
+    }
     public static void main(String[] args) {
         Random random = new Random();
         int cartela[][] = new int[5][5];
@@ -22,17 +34,5 @@ public class Exerc_Matrizes_Random {
             }
             System.out.println();
         }
-    }
-    public static boolean verificarRep(int[][] cartela, int num, int linha, int coluna) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (i < linha || (i == linha && j < coluna)) {
-                    if (cartela[i][j] == num) {
-                        return true; //achou algum repetido
-                    }
-                }
-            }
-        }
-        return false; // o numero não repitiu 
     }
 }
